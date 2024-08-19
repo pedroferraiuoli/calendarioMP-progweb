f19e99a309f8c637645d060034cbe150e7b71e5d
Pedro Ferraiuoli <92264274+pedroferraiuoli@users.noreply.github.com>
2024-08-19 10:50:24 -0300

    Spring

---
+++ src/test/java/com/calendario/siscalen/SiscalenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendario.siscalen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiscalenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
